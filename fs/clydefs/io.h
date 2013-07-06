298f5765e6f096c9858fa3b14750dd527f66f43b
Jesper Madsen <jmad@itu.dk>
2013-07-06 17:29:35 +0200

    begun stubbing out some of the IO commands expected to form the bridge to the device. Will need to refine these and expand AoE driver to branch on new custom commands most likely carried via bi_private in the bio.

---
+++ fs/clydefs/io.h
@@ -0,0 +1,22 @@
+#ifndef __CLYDEFS_IO_H
+#define __CLYDEFS_IO_H
+#include <linux/types.h>
+
+/*TODO: set a better value, check that backend will never return 0 as a valid tree/node id*/
+#define BIO_ALLOC_FAIL 0
+
+int clydefs_io_init(void);
+
+void clydefs_io_exit(void);
+
+u64 clydefs_io_create_tree(void);
+
+void clydefs_io_remove_tree(void);
+
+u64 clydefs_io_insert(u64 tid, u64 len, void *data);
+
+void clydefs_io_remove(u64 tid, u64 nid);
+
+void clydefs_io_update(u64 tid, u64 nid, u64 offset, u64 len, void *data);
+
+#endif //__CLYDEFS_IO_H
